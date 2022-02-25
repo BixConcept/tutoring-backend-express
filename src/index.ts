4fb1c4236f118c1e5c56bc669866fbf7ae8fdd76
3nt3 <gott@3nt3.de>
2022-02-25 16:45:26 +0100

    fix type casting again
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- src/index.ts
+++ src/index.ts
@@ -69,12 +69,7 @@ export const pool = mysql.createConnection({
     // We only want to cast tinyint fields that have a single-bit in them. If the field
     // has more than one bit, then we cannot assume it is supposed to be a Boolean.
     if (field.type === "TINY" && field.length === 1) {
-      var bytes = field.buffer();
-
-      // A Buffer in Node represents a collection of 8-bit unsigned integers.
-      // Therefore, our single "bit field" comes back as the bits '0000 0001',
-      // which is equivalent to the number 1.
-      return bytes[0] === 1;
+      return field.string() === "1";
     }
 
     return useDefaultTypeCasting();
