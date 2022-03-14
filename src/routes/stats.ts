d7e6e321b85d21683369e82c07c4579ec3c88e47
3nt3 <gott@3nt3.de>
2022-03-14 03:28:31 +0100

    add ?aggregate to /apirequests
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- src/routes/stats.ts
+++ src/routes/stats.ts
@@ -8,18 +8,56 @@ export const getApiRequests = (req: express.Request, res: express.Response) => {
     return res.status(401).json({ msg: "unauthorized" });
   }
   if (req.user.authLevel >= AuthLevel.Verified) {
-    pool.query("SELECT * FROM apiRequest", [], (err: any, results: any[]) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ msg: "internal server error" });
-      }
+    pool.query(
+      "SELECT * FROM apiRequest ORDER BY time ASC",
+      [],
+      (err: any, results: any[]) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ msg: "internal server error" });
+        }
 
-      // remove everything except id and date
-      if ((req.user?.authLevel || 0) < AuthLevel.Admin) {
-        results = results.map((r) => ({ id: r.id, time: r.time }));
+        // remove everything except id and date
+        if ((req.user?.authLevel || 0) < AuthLevel.Admin) {
+          results = results.map((r) => ({ id: r.id, time: r.time }));
+        }
+
+        // if you pass a query parameter named 'aggregate' with the value of seconds each interval should have, we will aggregate on the server-side which *massively* reduces the amount of data being sent in most reasonable cases
+        // example values: 3600 (one hour in seconds) - 86400 (one day in seconds) - whatever, you get the system
+        if (req.query.aggregate && typeof req.query.aggregate === "string") {
+          // convert to milliseconds
+          const interval = parseInt(req.query.aggregate) * 1000;
+          if (isNaN(interval)) {
+            return res
+              .status(400)
+              .json({ msg: "?aggregate must be an integer" });
+          }
+
+          // difference between first and last request in seconds
+          const delta =
+            results[results.length - 1].time.getTime() -
+            results[0].time.getTime();
+
+          const n = Math.floor(delta / interval);
+
+          const first = results[0].time;
+
+          const times = new Array(n)
+            .fill(null)
+            .map((_, i) => first.getTime() + i * interval);
+
+          const values = times.map((value: number) => ({
+            time: value,
+            value: results.filter(
+              (x) => x.time > value && x.time.getTime() < value + interval
+            ).length,
+          }));
+          return res.json({ content: values });
+        } else {
+          return res.json({ content: results });
+        }
       }
-      return res.json({ content: results });
-    });
+    );
   } else return res.status(403).json({ msg: "forbidden" });
 };
 
